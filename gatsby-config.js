ba3c5f83b68afbe9488c39bd01e05e6d6ef750ae
deeptimahesh <deepti.mj24@gmail.com>
2024-10-22 23:53:01 -0700

    feat: Images display correctly

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,8 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
+              linkImagesToOriginal: false, // Prevents linking images to the original
+              pathPrefix: "/leloop",
             },
           },
         ],
